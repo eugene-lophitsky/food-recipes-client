64bd49e3b624e58e0d09b42ea6f36bc2fd12f817
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-02-23 08:36:52 +0300

    add routes

--- src/app/routes/routes.jsx
+++ src/app/routes/routes.jsx
@@ -1,5 +1,9 @@
 import AddRecipePage from "../../pages/add-recipe/ui/AddRecipePage.jsx";
 import {RecipesListPage} from "../../pages/recipes-list/ui/index.js";
+import {RecipePage} from "../../pages/recipe/ui/index.js";
+import {CategoriesPage} from "../../pages/categories/ui/index.js";
+import {UsersPage} from "../../pages/users/ui/index.js";
+import {SettingsPage} from "../../pages/settings/ui/index.js";
 
 const routes = [
   {
@@ -9,6 +13,22 @@ const routes = [
   {
     path: "/add-recipe",
     element: <AddRecipePage />,
+  },
+  {
+    path: "/recipe",
+    element: <RecipePage />,
+  },
+  {
+    path: "/categories",
+    element: <CategoriesPage />,
+  },
+  {
+    path: "/users",
+    element: <UsersPage />,
+  },
+  {
+    path: "/settings",
+    element: <SettingsPage />,
   },
 ];
 
