d11a7e5a3aebfeeb22fa58d38a2a37f91f88b33c
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-02-24 23:49:41 +0300

    add state to burgerButton

--- src/widgets/header/ui/Header.jsx
+++ src/widgets/header/ui/Header.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+// import * as React from 'react';
+import {useState} from "react";
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
@@ -7,17 +8,18 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import styles from "./Header.module.css";
+import buttonDefault from "./buttonDefault.jpg";
+import buttonPressed from "./buttonPressed.jpg";
+
 
 export default function Header() {
-    const [open, setOpen] = React.useState(false);
-
-    const toggleDrawer = (newOpen) => () => {
-        setOpen(newOpen);
-    };
+    const [open, setOpen] = useState();
 
     const DrawerList = (
         <div className={styles.drawerWrapper}>
-            <Box className={styles.box} sx={{ width: 250}} role="presentation" onClick={toggleDrawer(false)}>
+            <Box className={styles.box} sx={{ width: 250}} role="presentation"
+                 // onClick={toggleDrawer(false)}
+            >
                 <List>
                     {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
                         <ListItem key={text} disablePadding>
@@ -32,17 +34,20 @@ export default function Header() {
     );
 
     return (
-        <div className={styles.drawerBody}>
-            <header className={styles.header}>
-                <div className={styles.header__title}>My test header</div>
-                <div className={styles.burger}>
-                    <span></span>
-                </div>
-            </header>
-            <Button onClick={toggleDrawer(true)}>Open drawer</Button>
-            <Drawer open={open} onClose={toggleDrawer(false)}>
-                {DrawerList}
-            </Drawer>
-        </div>
+      <div className={styles.drawerBody}>
+        <header className={styles.header}>
+          <button style={open ? {cursor: "pointer"} : {cursor: "default"}} id="burgerButton" onClick={()=>{
+              open ? setOpen(false) : setOpen(true)}}>
+            <img src={open ? buttonDefault : buttonPressed} />
+          </button>
+          <div className={styles.header__title}>My test header</div>
+        </header>
+        <Button >Open drawer</Button>
+        <Drawer open={open}
+                // onClose={toggleDrawer(false)}
+        >
+          {DrawerList}
+        </Drawer>
+      </div>
     );
 }
