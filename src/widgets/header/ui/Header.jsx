bf5ea4ad6d3bd821245aac3b3e4fcbb74ed4e319
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-02-25 09:52:54 +0300

    wip

--- src/widgets/header/ui/Header.jsx
+++ src/widgets/header/ui/Header.jsx
@@ -1,8 +1,6 @@
-// import * as React from 'react';
 import {useState} from "react";
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -11,22 +9,26 @@ import styles from "./Header.module.css";
 import buttonDefault from "./buttonDefault.jpg";
 import buttonPressed from "./buttonPressed.jpg";
 
-
 export default function Header() {
     const [open, setOpen] = useState();
 
+    const toggleDrawer = () => {
+        // open ? setOpen(false) : setOpen(true)
+        setOpen(!open);
+    }
+
     const DrawerList = (
         <div className={styles.drawerWrapper}>
-            <Box className={styles.box} sx={{ width: 250}} role="presentation"
-                 onClick={()=>{
-                     setOpen(false)
+            <Box className={styles.box} sx={{width: 250}} role="presentation"
+                 onClick={() => {
+                     setOpen(!open)
                  }}
             >
                 <List>
                     {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
                         <ListItem key={text} disablePadding>
                             <ListItemButton>
-                                <ListItemText primary={text} />
+                                <ListItemText primary={text}/>
                             </ListItemButton>
                         </ListItem>
                     ))}
@@ -36,20 +38,22 @@ export default function Header() {
     );
 
     return (
-      <div className={styles.drawerBody}>
-        <header className={styles.header}>
-          <button style={open ? {cursor: "pointer"} : {cursor: "default"}} id="burgerButton" onClick={()=>{
-              open ? setOpen(false) : setOpen(true)}}>
-            <img src={open ? buttonDefault : buttonPressed} alt="" />
-          </button>
-          <div className={styles.header__title}>My test header</div>
-        </header>
-        <Button >Open drawer</Button>
-        <Drawer open={open}
-                // onClose={toggleDrawer(false)}
-        >
-          {DrawerList}
-        </Drawer>
-      </div>
+        <div className={styles.drawerBody}>
+            <header className={styles.header}>
+                <button style={open ? {cursor: "pointer"} : {cursor: "default"}} id="burgerButton" onClick={toggleDrawer}>
+                    <img src={open ? buttonDefault : buttonPressed} alt=""/>
+                </button>
+                <div className={styles.header__title}>My test header</div>
+            </header>
+
+            <Drawer open={open}>{DrawerList}</Drawer>
+        </div>
     );
 }
+
+
+// <button style={open ? {cursor: "pointer"} : {cursor: "default"}} id="burgerButton" onClick={() => {
+//     open ? setOpen(false) : setOpen(true)
+// }}>
+//     <img src={open ? buttonDefault : buttonPressed} alt=""/>
+// </button>
