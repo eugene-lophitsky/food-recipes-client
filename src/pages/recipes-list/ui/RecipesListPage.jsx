dc4b144aaf70cc7350ca84a9b235cde3c0a90c93
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-02-27 20:26:20 +0300

    добавлены стили ссылок в Navigation

--- src/pages/recipes-list/ui/RecipesListPage.jsx
+++ src/pages/recipes-list/ui/RecipesListPage.jsx
@@ -2,11 +2,7 @@ const RecipesListPage = () => {
   return (
     <div>
       <h2>Recipes List Page</h2>
-
       <div>
-        <button>Text</button>
-        <button>Contained</button>
-        <button>Outlined</button>
       </div>
     </div>
   );
