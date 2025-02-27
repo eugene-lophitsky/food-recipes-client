4a9e0cb04f33f827d4231b27b50df336ad9e036f
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-02-27 21:39:36 +0300

    add form

--- src/pages/add-recipe/ui/AddRecipePage.jsx
+++ src/pages/add-recipe/ui/AddRecipePage.jsx
@@ -1,5 +1,29 @@
+import styles from "./AddRecipePage.module.css"
+
 const AddRecipePage = () => {
-  return <div>AddRecipe Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Добавление рецепта</div>
+      <form className={styles.formBody}>
+        <label htmlFor="name">Название</label>
+        <input className={styles.recipeName} id="name" type="text" placeholder="Введите название" />
+        <label htmlFor="description">Описание</label>
+        <textarea
+          className={styles.description}
+          name="description"
+          placeholder="Опишите способ приготовления"
+          cols={40}
+          rows={7}
+        />
+
+        <span className={styles.ingredientWrapper}>
+            <a href="#" className={styles.ingredientButton}></a>
+          <input className={styles.ingredient} type="text" />
+        </span>
+        <button className={styles.saveButton}>Сохранить</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddRecipePage;
