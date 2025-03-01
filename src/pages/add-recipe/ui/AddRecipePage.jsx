9c3796736037ee9aa0f5239d57aa4e6c1a0f6bb5
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-03-01 16:11:25 +0300

    add validation for 2 fields

--- src/pages/add-recipe/ui/AddRecipePage.jsx
+++ src/pages/add-recipe/ui/AddRecipePage.jsx
@@ -1,21 +1,20 @@
 import styles from "./AddRecipePage.module.css";
-import {useFormik} from "formik";
+import { useFormik } from "formik";
 
-const validate = values => {
+const validate = (values) => {
   const errors = {};
   if (!values.recipeName) {
-    errors.recipeName = 'Поле ввода пустое';
+    errors.recipeName = "Поле ввода пустое";
   } else if (values.recipeName.length < 10) {
-    errors.recipeName = 'Введите не менее 10 символов';
+    errors.recipeName = "Введите не менее 10 символов";
   }
   if (!values.description) {
-    errors.description = 'Поле ввода пустое';
+    errors.description = "Поле ввода пустое";
   } else if (values.description.length < 20) {
-    errors.description = 'Описание не может содержать менее 20 символов';
+    errors.description = "Описание не может содержать менее 20 символов";
   }
   return errors;
 };
-
 
 const AddRecipePage = () => {
   const formik = useFormik({
@@ -24,7 +23,7 @@ const AddRecipePage = () => {
       description: "",
     },
     validate,
-  })
+  });
 
   return (
     <div className={styles.container}>
@@ -32,36 +31,42 @@ const AddRecipePage = () => {
       <form className={styles.formBody} onSubmit={formik.handleSubmit}>
         <label htmlFor="name">Название</label>
         <input
-            className={styles.recipeName}
-            id="recipeName"
-            name="recipeName"
-            type="text"
-            placeholder="Введите название"
-            value={formik.values.recipeName}
-            onChange={formik.handleChange}
+          className={styles.recipeName}
+          id="recipeName"
+          name="recipeName"
+          type="text"
+          placeholder="Введите название"
+          value={formik.values.recipeName}
+          onChange={formik.handleChange}
         />
-        <div className={styles.recipeNameError}>{formik.errors.recipeName ? formik.errors.recipeName : null}</div>
+        <div className={styles.recipeNameError}>
+          {formik.errors.recipeName ? formik.errors.recipeName : null}
+        </div>
         <label htmlFor="description">Описание</label>
         <textarea
-            className={styles.description}
-            id="description"
-            name="description"
-            placeholder="Опишите способ приготовления"
-            value={formik.values.description}
-            onChange={formik.handleChange}
-            cols={40}
-            rows={7}
+          className={styles.description}
+          id="description"
+          name="description"
+          placeholder="Опишите способ приготовления"
+          value={formik.values.description}
+          onChange={formik.handleChange}
+          cols={40}
+          rows={7}
         />
-        <div className={styles.descriptionError}>{formik.errors.description ? formik.errors.description : null}</div>
+        <div className={styles.descriptionError}>
+          {formik.errors.description ? formik.errors.description : null}
+        </div>
         <span className={styles.ingredientWrapper}>
           <a href="#" className={styles.ingredientButton}></a>
           <input
-              className={styles.ingredient}
-              type="text"
-              placeholder="Ингредиент"
+            className={styles.ingredient}
+            type="text"
+            placeholder="Ингредиент"
           />
         </span>
-        <button type="submit" className={styles.saveButton}>Сохранить</button>
+        <button type="submit" className={styles.saveButton}>
+          Сохранить
+        </button>
       </form>
     </div>
   );
